1f8c5f449599d502b9e140a3447ce241645399be
saitej07 <saitejabollabathini@gmail.com>
2025-08-23 20:35:05 +0530

    Removed all security related things

--- src/main/java/com/saiteja/ProductService/controller/ProductController.java
+++ src/main/java/com/saiteja/ProductService/controller/ProductController.java
@@ -7,7 +7,6 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,21 +16,18 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    //@PreAuthorize("hasAnyRole('Admin')")
     @PostMapping
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
         long productId = productService.addProduct(productRequest);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
 
-    //@PreAuthorize("hasAnyRole('Admin', 'Customer')")
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
         ProductResponse productResponse = productService.getProductById(productId);
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
-    //@PreAuthorize("hasAnyRole('Admin', 'Customer')")
     @PutMapping("/reduceQuantity/{id}")
     public ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity) {
         productService.reduceQuantity(productId, quantity);
--- src/main/java/com/saiteja/ProductService/security/WebSecurityConfig.java
+++ src/main/java/com/saiteja/ProductService/security/WebSecurityConfig.java
@@ -1,52 +1,52 @@
-package com.saiteja.ProductService.security;
-
-import org.hibernate.sql.ast.tree.expression.Collation;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequest ->
-                        authorizeRequest.anyRequest().authenticated())
-                .oauth2ResourceServer(oAuth2 -> oAuth2.jwt(
-                        jwt -> jwt.jwtAuthenticationConverter(converter())
-                ));
-        return http.build();
-    }
-
-    @Bean
-    public JwtAuthenticationConverter converter() {
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            JwtGrantedAuthoritiesConverter defaultConverter =new JwtGrantedAuthoritiesConverter();
-            Collection<GrantedAuthority> authorities = defaultConverter.convert(jwt);
-            Collection<GrantedAuthority> customAuthorities =
-                    jwt.getClaimAsStringList("https://saitejabuffer.com/roles")
-                            .stream()
-                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                            .collect(Collectors.toList());
-            authorities.addAll(customAuthorities);
-            return authorities;
-        });
-        return converter;
-    }
-}
+//package com.saiteja.ProductService.security;
+//
+//import org.hibernate.sql.ast.tree.expression.Collation;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import java.util.Collection;
+//import java.util.stream.Collectors;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//public class WebSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(authorizeRequest ->
+//                        authorizeRequest.anyRequest().authenticated())
+//                .oauth2ResourceServer(oAuth2 -> oAuth2.jwt(
+//                        jwt -> jwt.jwtAuthenticationConverter(converter())
+//                ));
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public JwtAuthenticationConverter converter() {
+//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//            JwtGrantedAuthoritiesConverter defaultConverter =new JwtGrantedAuthoritiesConverter();
+//            Collection<GrantedAuthority> authorities = defaultConverter.convert(jwt);
+//            Collection<GrantedAuthority> customAuthorities =
+//                    jwt.getClaimAsStringList("https://saitejabuffer.com/roles")
+//                            .stream()
+//                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+//                            .collect(Collectors.toList());
+//            authorities.addAll(customAuthorities);
+//            return authorities;
+//        });
+//        return converter;
+//    }
+//}
